7b483d88fa4ea7ddfb7c9ce718b945d17d58b606
Luke Rogerson <lukerogerson@aol.com>
2019-11-16 12:31:06 +0000

    Create super cool app name generator

---
+++ android/app/src/main/kotlin/com/example/super_cool_thingy/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.super_cool_thingy
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
